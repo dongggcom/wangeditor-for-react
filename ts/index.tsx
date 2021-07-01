a2e70ca821bc45061bb46d0b66027c1f4f4d5f7a
dongmin <simon-dong@qq.com>
2021-07-01 10:28:45 +0800

    chore: 修改依赖

--- ts/index.tsx
+++ ts/index.tsx
@@ -2,5 +2,18 @@
  * @Author: donggg
  * @LastEditors: donggg
  * @Date: 2021-06-30 11:48:31
- * @LastEditTime: 2021-06-30 11:48:32
+ * @LastEditTime: 2021-07-01 10:28:27
  */
+import React, { FC } from 'react';
+const name: string = 'zs';
+
+export const age: number = 100;
+// export var SomeVar: { a: SomeType };
+
+[1, 2, 3].forEach(i => console.log(i))
+
+export const Hello: FC = () => {
+  return <p>hello world!</p>
+}
+
+export default name;
